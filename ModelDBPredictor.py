22205d4cfac898f63b99fbe35c722271ffde114f
SafwanShaikh <safwanshaikh456@hotmail.com>
2017-11-28 23:00:22 +0500

    after calculating, commit

--- ModelDBPredictor.py
+++ ModelDBPredictor.py
@@ -11,5 +11,59 @@ for i in range(len(ITC)):
     AllGPAs.append(ITC[i])
     AllGPAs.append(CP[i])
     AllGPAs.append(DS[i])
-    AllGPAs.append(DB[i])
 GPAs = np.array(AllGPAs)
+
+for i in range(len(GPAs)):
+    if GPAs[i] == 1.33:
+        GPAs[i] = 2
+    elif GPAs[i] == 1.67:
+        GPAs[i] = 3
+    elif GPAs[i] == 2.0:
+        GPAs[i] = 4
+    elif GPAs[i] == 2.33:
+        GPAs[i] = 5
+    elif GPAs[i] == 2.67:
+        GPAs[i] = 6
+    elif GPAs[i] == 3.0:
+        GPAs[i] = 7
+    elif GPAs[i] == 3.33:
+        GPAs[i] = 8
+    elif GPAs[i] == 3.67:
+        GPAs[i] = 9
+    elif GPAs[i] == 4.0:
+        GPAs[i] = 10
+GPAs = GPAs.reshape(int(len(AllGPAs)/3), 3)
+
+for i in range(len(DB)):
+    if DB[i] == 1.33:
+        DB[i] = 2
+    elif DB[i] == 1.67:
+        DB[i] = 2
+    elif DB[i] == 2.0:
+        DB[i] = 2
+    elif DB[i] == 2.33:
+        DB[i] = 3
+    elif DB[i] == 2.67:
+        DB[i] = 3
+    elif DB[i] == 3.0:
+        DB[i] = 3
+    elif DB[i] == 3.33:
+        DB[i] = 4
+    elif DB[i] == 3.67:
+        DB[i] = 4
+    elif DB[i] == 4.0:
+        DB[i] = 4
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+X_trainCP = np.array(GPAs[0:210])
+Y_trainCP = np.array(DB[0:210])
+clf.fit(X_trainCP, Y_trainCP)
+X_test = np.array(GPAs[-89:])
+Y_test = np.array(DB[-89:])
+Y_test = Y_test.ravel()
+prediction = clf.predict(X_test)
+prediction = prediction.ravel()
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Y_test, prediction))
+print(accuracy_score(Y_test, prediction, normalize=False)) #If False, return the number of correctly classified samples. Otherwise, return the fraction of correctly classified samples.
